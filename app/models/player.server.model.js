647eb1ba774d98c51a5b8392ba4d7827562b0232
Damon Emde <damon.emde@gmail.com>
2015-08-24 15:18:09 -0400

    initial set up, added roster and player server models

---
+++ app/models/player.server.model.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Player Schema
+ */
+var PlayerSchema = new Schema({
+	// Player model fields   
+	// ...
+});
+
+mongoose.model('Player', PlayerSchema);
---
+++ app/models/roster.server.model.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Roster Schema
+ */
+var RosterSchema = new Schema({
+	// Roster model fields   
+	// ...
+});
+
+mongoose.model('Roster', RosterSchema);
